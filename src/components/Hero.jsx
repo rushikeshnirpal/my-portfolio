d6c8d0d87a28c890dc9a1e5f01a6b5fb4c6dde3e
rushikeshnirpal <rushikeshnirpal22@gmail.com>
2026-03-27 18:41:32 +0530

    added profile image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,32 @@
+import { TypeAnimation } from 'react-type-animation';
 import profile from "../assets/profile.png";
 
 function Hero() {
   return (
     <div id="home" data-aos="zoom-in" className="text-center mt-24 px-4">
 
-      {/* 👇 IMAGE FIRST */}
+      {/* IMAGE */}
       <img 
         src={profile} 
         alt="profile"
         className="w-32 h-32 mx-auto rounded-full object-cover shadow-lg border-4 border-blue-500"
       />
 
-      <h1 className="text-5xl md:text-6xl font-bold leading-tight mt-6">
-        Hi, I'm <span className="text-blue-500">Rushikesh</span> 👋
-      </h1>
+      {/* 👇 CORRECT */}
+      <TypeAnimation
+        sequence={[
+          "Hi, I'm Rushikesh 👋",
+          2000,
+          "Frontend Developer 💻",
+          2000,
+          "Software Tester 🧪",
+          2000,
+        ]}
+        wrapper="h1"
+        speed={50}
+        repeat={Infinity}
+        className="text-5xl md:text-6xl font-bold mt-6"
+      />
 
       <p className="text-xl mt-4 text-gray-600">
         Frontend Developer & Software Tester
